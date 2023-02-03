bf59a7bd7319187ca0e34025918b9f31a9b3faea
BenSiv <bensiv92@gmail.com>
2023-02-03 14:24:46 +0200

    added prep and split

--- src/PreProccessing.py
+++ src/PreProccessing.py
@@ -2,79 +2,81 @@
 Prep of the train and test datasets
 """
 
-PROJECT_DIR = "/home/bensiv/Documents/ITC/Main/FinalProject/ICBA_Project/"
-
 # packages
 import os
+import sys
 import numpy as np
 import pandas as pd
 from datetime import datetime
 
-def main():
+def main(PROJECT_DIR):
     """program skeleton"""
 
     # targets: lab results
-    # features: feed + temps
-    lab_results = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Raw","LabResults.csv"))
-    feed = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Raw","Feed.csv"))
-    temps = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Others","Temps.csv"))
+    # features: feed
+    lab_results = pd.read_csv(os.path.join(PROJECT_DIR,"data", "raw", "LabResults.csv"))
+    feed = pd.read_csv(os.path.join(PROJECT_DIR,"data", "raw", "Feed.csv"))
 
     # converting Date to datetime
-    for df in [lab_results, feed, temps]:
-        df['Date'] = pd.to_datetime(df['Date'])#, unit="D")
+    for df in [lab_results, feed]:
+        df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
 
-    # Omitting Non-Dairy cattle groups from the data
+    # omitting Non-Dairy cattle groups from the data
     feed = feed[feed.Group_name.str.match("Dairy")]
 
-    # Converting the quantities to be per cow
+    # converting the quantities to be per cow
     feed.Quantity = feed.Quantity / feed.Num_of_cows
 
-    #  combining features first
-    features = pd.merge(feed, temps, on="Date")
+    # filtering the feed dataframe to the measurments only
+    feed_measurments = pd.read_csv(os.path.join(PROJECT_DIR,"data", "others", "Feed_measure.csv"))
+    feed_measurments.drop(columns=["Unnamed: 0"], inplace=True)
+    features = feed.merge(feed_measurments)
 
     # assign sample groups based on the date of the sample
     sample_dates = lab_results.Date.unique()
     sample_dates_df = pd.DataFrame({"Date":sample_dates, "Sample":range(1,len(sample_dates)+1)})
     targets = pd.merge(lab_results, sample_dates_df, on="Date")
+    targets.drop(columns=["Somatic_cells","Group"], inplace=True)
+    targets = targets.groupby("Date").mean().reset_index()
     
+    # average all groups by date 
+    feed_measure_dict = features.set_index("Feed_ID")["Feed_name"].drop_duplicates().to_dict()
+    features.drop(columns=["Group","Group_name","Feed_num","Feed_name","Units","Num_of_cows"], inplace=True)
+    features = features.groupby(["Date","Feed_ID"]).mean().reset_index()
 
     sample_range = np.array([]).astype(np.datetime64)
     sample_num = np.array([]).astype(np.int64)
+    day_from_sample = np.array([]).astype(np.int64)
     week = 7 # days
     for (num, date) in enumerate(sample_dates):
         sample_range = np.concatenate([sample_range, pd.date_range(start=date, periods=week)])
         sample_num = np.concatenate([sample_num, np.array([num]*(week))])
+        day_from_sample = np.concatenate([day_from_sample, np.arange(week)])
 
-    sample_range_df = pd.DataFrame({"Date":sample_range, "Sample":sample_num})
+    sample_range_df = pd.DataFrame({"Date":sample_range, "Sample":sample_num, "Day_from_sample" : day_from_sample})
 
-    features = pd.merge(features, sample_range_df, on="Date", how="left")
+    features = pd.merge(features, sample_range_df, on="Date")
+    # features.drop(columns=["Sample"], inplace=True)
+    features_wide = features.pivot(index="Sample", columns=["Day_from_sample","Feed_ID"], values="Quantity")
+    features_wide.fillna(0, inplace=True)
+    features_wide.reset_index(inplace=True)
 
-    features.dropna(subset=["Sample"], inplace=True)
-
+    targets.drop(columns=["Date"],inplace=True)
     # combining features with targets
-    data = pd.merge(features, targets, on=["Group", "Sample"], how="left")
+    data = pd.merge(features_wide, targets, on="Sample", how="left")
+    data.drop(columns=[data.columns[1]], inplace=True)
 
-    col_to_drop = ["Somatic_cells", "Group_name", "Units", "Num_of_cows", "Feed_name", "Feed_num", "Date_y"]
-    data.drop(columns=col_to_drop, inplace=True)
-    data.rename(columns={"Date_x" : "Date"}, inplace=True)
-
-    data.Feed_ID = [f"feed{i}" for i in data.Feed_ID]
-    
-    cols_to_stack = ["MaxTemp", "MinTemp"]
-    cols_not_to_stack = [col for col in data.columns if col not in cols_to_stack]
-
-    temp_stacked = (
-        data
-        .drop(columns=["Feed_ID", "Quantity"])
-        .set_index(cols_not_to_stack)
-        .stack()
-        .reset_index()
-        .rename(columns={"level_9" : "Feature", 0 : "Value"})
-    )
-    temp_stacked = data[["MaxTemp", "MinTemp"]].stack()
+    data.to_csv(os.path.join(PROJECT_DIR,"data", "combined", "WeekBeforeSample.csv"))
 
 
 if __name__ == "__main__":
-    main()
+    # PROJECT_DIR = "/home/bensiv/Documents/ITC/Main/FinalProject/ICBA_Project/PySrc/icba_project"
+    args = sys.argv
+    if len(args) == 1:
+        PROJECT_DIR = os.getcwd()
+    else:
+        PROJECT_DIR = args[1]
+
+    main(PROJECT_DIR)
 
 
--- src/SplitTrainTest.py
+++ src/SplitTrainTest.py
@@ -2,10 +2,10 @@
 Prep of the train and test datasets
 """
 
-PROJECT_DIR = "/home/bensiv/Documents/ITC/Main/FinalProject/ICBA_Project/"
 
 # packages
 import os
+import sys
 import numpy as np
 import pandas as pd
 from datetime import datetime
@@ -17,55 +17,38 @@ def get_days_prior(date, period):
 def main():
     """program skeleton"""
 
-    # targets: lab results
-    # features: feed + temps
-    lab_results = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Raw","LabResults.csv"))
-    feed = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Raw","Feed.csv"))
-    temps = pd.read_csv(os.path.join(PROJECT_DIR,"CSVs", "Others","Temps.csv"))
+    WeekBeforeSample = pd.read_csv(os.path.join(PROJECT_DIR,"data", "combined", "WeekBeforeSample.csv"))
+    WeekBeforeSample.drop(columns=["Unnamed: 0"], inplace=True)
 
-    # converting Date to datetime
-    for df in [lab_results, feed, temps]:
-        df['Date'] = pd.to_datetime(df['Date'])#, unit="D")
+    X = WeekBeforeSample.drop(columns=["Milk","Fat","Protein","Lactose"])
+    y = WeekBeforeSample[["Sample","Milk","Fat","Protein","Lactose"]]
 
-    # Omitting Non-Dairy cattle groups from the data
-    feed = feed[feed.Group_name.str.match("Dairy")]
+    # train 60%, validation 20%, test, 20%
+    seps = np.linspace(0,len(y),10).astype(int)
+    split_indices = {
+        "train" : (seps[0],seps[5]),
+        "val" : (seps[5],seps[7]),
+        "test" : (seps[7],seps[9])
+    }
 
-    # Converting the quantities to be per cow
-    feed.Quantity = feed.Quantity / feed.Num_of_cows
+    data_sets = dict()
+    for data_set in ["train", "val", "test"]:
+        start, end = split_indices[data_set]
+        data_sets[data_set] = WeekBeforeSample.iloc[start:end , :]
 
-    #  combining features first
-    features = pd.merge(feed, temps, on="Date")
+    for data_set, data in data_sets.items():
+        data.to_csv(os.path.join(PROJECT_DIR,"data", "splitted", f"{data_set}.csv"))
 
-    # assign sample groups based on the date of the sample
-    sample_dates = lab_results.Date.unique()
-    sample_dates_df = pd.DataFrame({"Date":sample_dates, "Sample":range(1,len(sample_dates)+1)})
-    targets = pd.merge(lab_results, sample_dates_df, on="Date")
-    
 
-    sample_range = np.array([]).astype(np.datetime64)
-    sample_num = np.array([]).astype(np.int64)
-    week = 7 # days
-    for (num, date) in enumerate(sample_dates):
-        sample_range = np.concatenate([sample_range, pd.date_range(start=date, periods=week)])
-        sample_num = np.concatenate([sample_num, np.array([num]*(week))])
-
-    sample_range_df = pd.DataFrame({"Date":sample_range, "Sample":sample_num})
-
-    features = pd.merge(features, sample_range_df, on="Date", how="left")
-
-    features.dropna(subset=["Sample"], inplace=True)
-
-    # combining features with targets
-    data = pd.merge(features, targets, on=["Group", "Sample"], how="left")
-
-    col_to_drop = ["Somatic_cells", "Group_name", "Units", "Num_of_cows", "Feed_name", "Feed_num", "Date_y"]
-    data.drop(columns=col_to_drop, inplace=True)
-    data.rename(columns={"Date_x" : "Date"}, inplace=True)
-
-    data.Feed_ID = [f"feed{i}" for i in data.Feed_ID]
-    
 
 if __name__ == "__main__":
-    main()
+    # PROJECT_DIR = "/home/bensiv/Documents/ITC/Main/FinalProject/ICBA_Project/PySrc/icba_project"
+    args = sys.argv
+    if len(args) == 1:
+        PROJECT_DIR = os.getcwd()
+    else:
+        PROJECT_DIR = args[1]
+
+    main(PROJECT_DIR)
 
 
